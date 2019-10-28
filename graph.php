0a2401cc1a8f2aa4f1e7252184941da2c8f2131f
rasm947f <54017710+rasm947f@users.noreply.github.com>
2019-10-28 09:02:05 +0100

    Changed draw function names
    Changed the drawChart function names to drawGraph and drawPie depending on what it draws. Did the same thing with the chart_div.
    Now graph_div and pie_div.

--- graph.php
+++ graph.php
@@ -12,7 +12,6 @@ if ($conn->connect_error) {
 
 $sql = "SELECT * FROM ShoeSize ORDER BY shoeSize DESC";
 $result = $conn->query($sql);
-
 ?>
 
 <!doctype html>
@@ -22,45 +21,45 @@ $result = $conn->query($sql);
         <title>Graf</title>
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script type="text/javascript">
-            google.charts.load('current', {'packages':['corechart']});
-            google.charts.setOnLoadCallback(drawChart);
-            
+            google.charts.load('current', {'packages': ['corechart']});
+            google.charts.setOnLoadCallback(drawGraph);
+
             // Function to generate a random color
             function getRandomColor() {
                 var letters = '0123456789ABCDEF';
                 var color = '#';
-                
+
                 for (var i = 0; i < 6; i++) {
                     color += letters[Math.floor(Math.random() * 16)];
                 }
                 return color;
             }
-            
+
             // Function to draw the google chart
-            function drawChart() {
+            function drawGraph() {
                 var data = new google.visualization.DataTable();
-                    data.addColumn('string', 'Name');
-                    data.addColumn('number', 'Skostørrelse');
-                    data.addRows([
+                data.addColumn('string', 'Name');
+                data.addColumn('number', 'Skostørrelse');
+                data.addRows([
 
-                    <?php
-                    // Insert the name and shoesize from a database via php
-                    if (mysqli_num_rows($result) > 0) {
+<?php
+// Insert the name and shoesize from a database via php
+if (mysqli_num_rows($result) > 0) {
 
-                    while ($row = mysqli_fetch_array($result)) {
-                        
-                    // This is the format that google charts require
-                    echo "['" . $row['name'] . "', " . $row['shoeSize'] . "],";
-                    }
-                }
-                ?>
+    while ($row = mysqli_fetch_array($result)) {
+
+        // This is the format that google charts require
+        echo "['" . $row['name'] . "', " . $row['shoeSize'] . "],";
+    }
+}
+?>
                 ]);
                 // Options for the chart
-                var options = {'title':'Skostørrelse',
-                       'colors': [getRandomColor(), '#228B22'],
-                       'width':1200,
-                       'height':500};
-                var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+                var options = {'title': 'Skostørrelse',
+                    'colors': [getRandomColor(), '#228B22'],
+                    'width': 1200,
+                    'height': 500};
+                var chart = new google.visualization.ColumnChart(document.getElementById('graph_div'));
                 chart.draw(data, options);
             }
         </script>
@@ -70,12 +69,11 @@ $result = $conn->query($sql);
         <div>
             <h1>Skostørrelse <a href="pie.php">(Pie)</a></h1>
         </div>
-        
-        
+
         <a href="shoes.html">Home</a>
         <a href="list.php">List</a>
         <a href="graph.php">Graph</a>
-        
-        <div id="chart_div"></div>
+
+        <div id="graph_div"></div>
     </body>
 </html>
--- pie.php
+++ pie.php
@@ -21,21 +21,21 @@ $result = $conn->query($sql);
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script type="text/javascript">
             google.charts.load('current', {'packages': ['corechart']});
-            google.charts.setOnLoadCallback(drawChart);
-            
+            google.charts.setOnLoadCallback(drawPie);
+
             // Function to generate a random color
             function getRandomColor() {
                 var letters = '0123456789ABCDEF';
                 var color = '#';
-                
+
                 for (var i = 0; i < 6; i++) {
                     color += letters[Math.floor(Math.random() * 16)];
                 }
                 return color;
             }
-            
+
             // Function to draw the google chart
-            function drawChart() {
+            function drawPie() {
                 var data = new google.visualization.DataTable();
                 data.addColumn('string', 'Skostørrelse');
                 data.addColumn('number', 'Hyppighed');
@@ -53,9 +53,8 @@ if (mysqli_num_rows($result) > 0) {
 
     // Use the data in a foreach loop
     foreach ($data as $row) {
-        
+
         //print_r($row['shoeSize']);
-        
         // Put each shoeSize into the values array
         $values[] = $row['shoeSize'];
 
@@ -64,33 +63,29 @@ if (mysqli_num_rows($result) > 0) {
     }
     //echo "Values:";
     //print_r($values);
-    
     // actually count the amount of eah shoesize and put them
     // into a new array called percent
     $percent = array_count_values($values);
-    
+
     // Time to print the data
     foreach ($data as $row) {
         // Reset $firstval to the first value in the multidimensional array
         $firstVal = reset($data);
 
         //echo $firstVal;
-        
         // Value equals shoesize
         $value = $row['shoeSize'];
-        
+
         // Only print one line for each shoesize
         // Don't print size 45 if you just printed it.
         // Otherwise, you'll get two many columns/slices of pie
         if ($value != $lastVal || $value == $firstVal) {
             //echo "( $value )";
             //print_r($percent["$value"]);
-            
             // Turn the size integer into a string
             $size = strval($value);
 
             //echo $size;
-            
             // Print the values in the format that g-charts require
             echo "['" . $size . "', " . $percent["$value"] . "],";
             $lastVal = $value;
@@ -106,7 +101,7 @@ if (mysqli_num_rows($result) > 0) {
                     pieSliceText: 'percentage',
                     'width': 1200,
                     'height': 550};
-                var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+                var chart = new google.visualization.PieChart(document.getElementById('pie_div'));
                 chart.draw(data, options);
             }
         </script>
@@ -121,6 +116,6 @@ if (mysqli_num_rows($result) > 0) {
         <a href="list.php">List</a>
         <a href="graph.php">Graph</a>
 
-        <div id="chart_div"></div>
+        <div id="pie_div"></div>
     </body>
 </html>
